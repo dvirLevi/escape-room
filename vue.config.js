9711d809dd98decd625571696b8ae73a79d833bb
dvirLevi <dvirleviapp@gmail.com>
2021-04-27 16:14:55 +0300

    add

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,27 @@
 // Inside vue.config.js
 module.exports = {
     // ...other vue-cli plugin options...
-    // pwa: {
-    //     name: "חדר בריחה | מרכז מורשת הרמב''ם ועדות ישראל",
-    //     themeColor: '#d80806',
-    //     msTileColor: '#d80806',
-    //     // workboxOptions: {
-    //     //     skipWaiting: true
-    //     // }
-    // }
     pwa: {
         name: "חדר בריחה | מרכז מורשת הרמב''ם ועדות ישראל",
         themeColor: '#d80806',
         msTileColor: '#d80806',
-        appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: 'black',
+        // workboxOptions: {
+        //     skipWaiting: true
+        // }
+    }
+//     pwa: {
+//         name: "חדר בריחה | מרכז מורשת הרמב''ם ועדות ישראל",
+//         themeColor: '#d80806',
+//         msTileColor: '#d80806',
+//         appleMobileWebAppCapable: 'yes',
+//         appleMobileWebAppStatusBarStyle: 'black',
     
-        // configure the workbox plugin
-        workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-          // swSrc is required in InjectManifest mode.
-          swSrc: 'src/registerServiceWorker.js',
-          // ...other Workbox options...
-        }
-      }
+//         // configure the workbox plugin
+//         workboxPluginMode: 'InjectManifest',
+//         workboxOptions: {
+//           // swSrc is required in InjectManifest mode.
+//           swSrc: 'src/registerServiceWorker.js',
+//           // ...other Workbox options...
+//         }
+//       }
 }
